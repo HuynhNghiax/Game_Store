f9d56ed4e37a88753f1a693f58613774ceb0d707
HuynhNghiax <146554474+HuynhNghiax@users.noreply.github.com>
2025-12-15 21:15:44 +0700

    chỉnh sửa app.jsx và library

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+// Import cả store và persistor (giờ file store.js đã có đủ cả 2)
+import { store, persistor } from './redux/store' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
