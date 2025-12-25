305cea42f4dcdbeb8db3ea56ab648f9adf5e4781
Phạm Quốc Phương Nam <22130175@st.hcmuaf.edu.vn>
2025-12-25 21:20:06 +0700

    Fix Profile Page

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,6 +1,5 @@
-import { useDispatch, useSelector } from 'react-redux';
-import type { TypedUseSelectorHook } from 'react-redux';
-import type { RootState, AppDispatch } from './store';
+import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
+import type { RootState, AppDispatch } from "./store";
 
 // Sử dụng 2 hook này trong toàn bộ ứng dụng thay vì useDispatch và useSelector gốc
 export const useAppDispatch: () => AppDispatch = useDispatch;
